14f4e9d1ce27c81366805adc57358dac80f2abe6
EndouKoumei <163804188+EndouKoumei@users.noreply.github.com>
2024-05-14 09:43:29 +0700

    Update and rename Dem_00_den_09_tren_LED_7_thanh.asm to Lab 10/Dem_00_den_09_tren_LED_7_thanh.asm

--- Lab 10/Dem_00_den_09_tren_LED_7_thanh.asm
+++ Lab 10/Dem_00_den_09_tren_LED_7_thanh.asm
@@ -1,35 +1,35 @@
-.eqv SEVENSEG_LEFT 0xFFFF0011    # ??a ch? c?a ?èn LED 7 ?o?n trái.
-.eqv SEVENSEG_RIGHT 0xFFFF0010   # ??a ch? c?a ?èn LED 7 ?o?n ph?i.
+.eqv SEVENSEG_LEFT 0xFFFF0011    # Dia chi cua den LED 7 doan trai.
+.eqv SEVENSEG_RIGHT 0xFFFF0010   # Dia chi cua den LED 7 doan phai.
 .data
 seven_seg_map: .byte 0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F # 0-9
 .text
 main:
-    li $t1, 0            # Bi?n ??m, kh?i t?o t? 0
+    li $t1, 0            # Bien dem, khoi tao tu 0
 count_loop:
-    move $t2, $t1        # Copy bi?n ??m vào $t2
+    move $t2, $t1        # Copy bien dem vao $t2
     div $t2, $t1, 10     # Chia $t1 cho 10
-    mfhi $t3             # L?y ph?n d? c?a phép chia (ch? s? hàng ??n v?)
-    mflo $t4             # L?y ph?n nguyên c?a phép chia (ch? s? hàng ch?c)
-    # Hi?n th? ch? s? hàng ch?c trên LED trái
+    mfhi $t3             # Lay phan du cua phep chia (cho so hang don vi)
+    mflo $t4             # Lay phan nguyÃªn cua phep chia (cho so hang chuc)
+    # Hien thi chu so hang chuc tren LED trai
     la $t0, seven_seg_map
     add $t0, $t0, $t4
     lb $a0, 0($t0)
     jal SHOW_7SEG_LEFT
-    # Hi?n th? ch? s? hàng ??n v? trên LED ph?i
+    # Hien thi chu so hang don vi tren LED phai
     la $t0, seven_seg_map
     add $t0, $t0, $t3
     lb $a0, 0($t0)
     jal SHOW_7SEG_RIGHT
-    # Thêm tr? 1 giây
+    # Them tre 1 giÃ¢y
     jal delay_1s
-    addi $t1, $t1, 1     # T?ng bi?n ??m
+    addi $t1, $t1, 1     # Tang bien dem
     li $t0, 10
-    bne $t1, $t0, count_loop # N?u ch?a ??m ??n 10 thì l?p l?i
+    bne $t1, $t0, count_loop # Neu chua dem den 10 thi lap lai
     j reset_loop
 #---------------------------------------------------------------
-# Hàm SHOW_7SEG_LEFT: B?t/t?t ?èn LED 7 ?o?n trái
-# param[in] $a0: giá tr? c?n hi?n th?
-# remark: $t0 thay ??i
+# HÃ m SHOW_7SEG_LEFT: Bat/tat den LED 7 doan trai
+# param[in] $a0: gia tri can hien thi
+# remark: $t0 thay doi
 #---------------------------------------------------------------
 SHOW_7SEG_LEFT:
     li $t0, SEVENSEG_LEFT
@@ -38,9 +38,9 @@ SHOW_7SEG_LEFT:
     jr $ra
     nop
 #---------------------------------------------------------------
-# Hàm SHOW_7SEG_RIGHT: B?t/t?t ?èn LED 7 ?o?n ph?i
-# param[in] $a0: giá tr? c?n hi?n th?
-# remark: $t0 thay ??i
+# HÃ m SHOW_7SEG_RIGHT: Bat/tat den LED 7 doan phai
+# param[in] $a0: gia tri can hien thi
+# remark: $t0 thay doi
 #---------------------------------------------------------------
 SHOW_7SEG_RIGHT:
     li $t0, SEVENSEG_RIGHT
@@ -49,12 +49,12 @@ SHOW_7SEG_RIGHT:
     jr $ra
     nop
 #---------------------------------------------------------------
-# Hàm delay_1s: T?o tr? kho?ng 1 giây b?ng syscall 32
-# remark: $a0, $v0 thay ??i
+# HÃ m delay_1s: Tao tre khoang 1 giay bang syscall 32
+# remark: $a0, $v0 thay doi
 #---------------------------------------------------------------
 delay_1s:
     li $v0, 32          # Syscall 32 (sleep)
-    li $a0, 1000        # Th?i gian tr? 1 giây (1000 ms)
+    li $a0, 1000        # Thoi gian tre 1 giÃ¢y (1000 ms)
     syscall
     jr $ra
     nop
